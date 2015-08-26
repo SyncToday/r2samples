e7745ea685b4b5789bb969ea56453535134a0c3a
davidpodhola <david.podhola@naseukoly.cz>
2015-08-26 16:08:04 +0200

    CopyContacts started

---
+++ src/Sync.Today.Samples.Exchange.CopyContacts/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Sync.Today.Samples.Exchange.CopyContacts")>]
+[<assembly: AssemblyProductAttribute("SyncToday2015R2.Samples")>]
+[<assembly: AssemblyDescriptionAttribute("Sample usage of Sync.Today 2015 R2")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
